05d48edb27793bd1189caf6e569f5aedfc4abaf7
erikplaza@gmail.com <erikplaza@gmail.com>
2019-05-04 13:43:31 -0700

    Got table to update and refresh on new db

--- models/example.js
+++ models/example.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+  var User = sequelize.define("User", {
+    username: DataTypes.STRING,
+    picks: DataTypes.TEXT
   });
-  return Example;
+  return User;
 };
